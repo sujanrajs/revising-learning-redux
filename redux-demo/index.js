994e8ad51510792b76791a6aa0353304d425e5cf
Sujan Raj Shrestha <sujanr.stha@gmail.com>
2022-08-29 23:07:01 +0300

    install and use logger middleware

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -2,6 +2,10 @@ const redux = require("redux");
 const createStore = redux.createStore;
 const bindActionCreators = redux.bindActionCreators;
 const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+
+const reduxLogger = require("redux-logger");
+const logger = reduxLogger.createLogger();
 
 const CAKE_ORDERED = "CAKE_ORDERED";
 const CAKE_RESTOCKED = "CAKE_RESTOCKED";
@@ -84,12 +88,14 @@ const rootReducer = combineReducers({
   iceCream: iceCreamReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log("Initial state", store.getState());
 
-const unsubscribe = store.subscribe(() =>
+/* const unsubscribe = store.subscribe(() =>
   console.log("update state", store.getState())
-);
+); // replaced by middleware */
+
+const unsubscribe = store.subscribe(() => {});
 
 const actions = bindActionCreators(
   { orderCake, restockCake, orderIceCream, restockIceCream },
