011a5626c3126d28f3022880238d989296b35208
Sujan Raj Shrestha <sujanr.stha@gmail.com>
2022-08-29 16:40:38 +0300

    create store and implement redux pattern - store, action and reducer

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,3 +1,6 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
 const CAKE_ORDERED = "CAKE_ORDERED";
 
 function orderCake() {
@@ -24,3 +27,16 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+const store = createStore(reducer);
+console.log("Initial state", store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log("update state", store.getState())
+);
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unsubscribe();
