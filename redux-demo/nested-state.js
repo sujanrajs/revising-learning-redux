dd7f03d4454ac444f8c2a28e4c32384dfdc2a7db
Sujan Raj Shrestha <sujanr.stha@gmail.com>
2022-08-29 20:31:22 +0300

    add new file to practice nested state

---
+++ redux-demo/nested-state.js
@@ -0,0 +1,44 @@
+const redux = require("redux");
+
+const initialState = {
+  name: "Sujan",
+  address: {
+    city: "Helsinki",
+    street: "H-Street 16 B",
+    Country: "Finland",
+  },
+};
+
+const STREET_UPDATED = "STREET_UPDATED";
+
+const updateStreet = (street) => {
+  return {
+    type: STREET_UPDATED,
+    payload: street,
+  };
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case STREET_UPDATED:
+      return {
+        ...state,
+        address: {
+          ...state.address,
+          street: action.payload,
+        },
+      };
+    default: {
+      return state;
+    }
+  }
+};
+
+const store = redux.createStore(reducer);
+console.log("Initial State", store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log("Update State", store.getState());
+});
+
+store.dispatch(updateStreet("H-Street 20 A"));
+unsubscribe();
