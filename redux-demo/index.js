2120ca8f0fbb493184e428c40fb8a2cf18b9f81a
Sujan Raj Shrestha <sujanr.stha@gmail.com>
2022-08-29 19:42:49 +0300

    add ice-cream actions and reducers

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -4,11 +4,27 @@ const bindActionCreators = redux.bindActionCreators;
 
 const CAKE_ORDERED = "CAKE_ORDERED";
 const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+const ICECREAM_ORDERED = "ICECREAM_ORDERED";
+const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED";
 
 function orderCake() {
   return {
     type: CAKE_ORDERED,
     payload: 1,
+  };
+}
+
+function orderIceCream(qty = 1) {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: qty,
+  };
+}
+
+function restockIceCream(qty = 1) {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: qty,
   };
 }
 
@@ -21,6 +37,7 @@ function restockCake(qty = 1) {
 
 const initialState = {
   numberOfCakes: 10,
+  numberOfIceCreams: 20,
 };
 
 // (previousState, action) => newState;
@@ -36,6 +53,16 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         numberOfCakes: state.numberOfCakes + action.payload,
+      };
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numberOfIceCreams: state.numberOfIceCreams - 1,
+      };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numberOfIceCreams: state.numberOfIceCreams + action.payload,
       };
     default:
       return state;
@@ -54,11 +81,18 @@ store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(restockCake(3)); */
 
-const actions = bindActionCreators({ orderCake, restockCake }, store.dispatch);
+const actions = bindActionCreators(
+  { orderCake, restockCake, orderIceCream, restockIceCream },
+  store.dispatch
+);
 
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(restockCake(3));
+
+actions.orderIceCream();
+actions.orderIceCream();
+actions.restockIceCream(2);
 
 unsubscribe();
