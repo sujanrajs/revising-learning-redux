2c189411193fa3f20456264b750199e042f8bcee
Sujan Raj Shrestha <sujanr.stha@gmail.com>
2022-08-29 16:59:41 +0300

    add redux helper function - bindActionCreators

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,5 +1,6 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const bindActionCreators = redux.bindActionCreators;
 
 const CAKE_ORDERED = "CAKE_ORDERED";
 const CAKE_RESTOCKED = "CAKE_RESTOCKED";
@@ -48,10 +49,16 @@ const unsubscribe = store.subscribe(() =>
   console.log("update state", store.getState())
 );
 
+/* store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
-store.dispatch(orderCake());
+store.dispatch(restockCake(3)); */
 
+const actions = bindActionCreators({ orderCake, restockCake }, store.dispatch);
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
 store.dispatch(restockCake(3));
 
 unsubscribe();
