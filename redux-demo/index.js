1849912f08645ce9d50ccbff9a5cda4cc42cd1ca
Sujan Raj Shrestha <sujanr.stha@gmail.com>
2022-08-29 16:23:04 +0300

    create reducer

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -6,3 +6,21 @@ function orderCake() {
     quantity: 1,
   };
 }
+
+const initialState = {
+  numberOfCakes: 10,
+};
+
+// (previousState, action) => newState;
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numberOfCakes: state.numberOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
