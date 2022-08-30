783d39f399796c00fb92f1937fa5c38aa37d752a
Sujan Raj Shrestha <sujanr.stha@gmail.com>
2022-08-30 10:18:07 +0300

    use of async actions using redux-toolkit (fetching data from API)

--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -2,6 +2,7 @@ const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
 const icecreamActions =
   require("./features/icecream/icecreamSlice").icecreamActions;
+const fetchUsers = require("./features/user/userSlice").fetchUsers;
 
 console.log("Initial state", store.getState());
 
@@ -16,7 +17,10 @@ const unsubscribe = store.subscribe(() => {
   console.log("Updated state", store.getState());
 });
 
-store.dispatch(cakeActions.ordered());
+store.dispatch(fetchUsers());
+
+// commented for asyncThunk (fetching: user)
+/* store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(3));
@@ -26,3 +30,4 @@ store.dispatch(icecreamActions.ordered());
 store.dispatch(icecreamActions.restocked(2));
 
 unsubscribe();
+ */
