b0e316e3c1b3d0eb0dec401e4cb16be999294b63
Sujan Raj Shrestha <sujanr.stha@gmail.com>
2022-09-01 10:10:39 +0300

    add input tag for user to input restock value in ice-cream

--- react-rtk-demo/src/features/icecream/IcecreamView.jsx
+++ react-rtk-demo/src/features/icecream/IcecreamView.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import { React, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { ordered, restocked } from "./icecreamSlice";
 
 export const IcecreamView = () => {
+  const [value, setValue] = useState(1);
   const numOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams);
   const dispatch = useDispatch();
   return (
     <div>
       <h2>Number of Ice Creams - {numOfIcecreams}</h2>
       <button onClick={() => dispatch(ordered())}>Order Ice Cream</button>
-      <button onClick={() => dispatch(restocked(3))}>Restock Ice Creams</button>
+      <input
+        type="number"
+        value={value}
+        onChange={(e) => setValue(parseInt(e.target.value))}
+      />
+      <button onClick={() => dispatch(restocked(value))}>
+        Restock Ice Creams
+      </button>
     </div>
   );
 };
