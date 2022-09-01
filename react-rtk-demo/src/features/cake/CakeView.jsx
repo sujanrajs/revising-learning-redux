a003a88c195468cbf8cec28dfc712c8d0d36e446
Sujan Raj Shrestha <sujanr.stha@gmail.com>
2022-09-01 09:41:08 +0300

    use of useSelector hook to read data from redux store

--- react-rtk-demo/src/features/cake/CakeView.jsx
+++ react-rtk-demo/src/features/cake/CakeView.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export const CakeView = () => {
+  const numOfCakes = useSelector((state) => state.cake.numOfCakes);
   return (
     <div>
-      <h2>Number of Cakes</h2>
+      <h2>Number of Cakes - {numOfCakes}</h2>
       <button>Order cake</button>
       <button>Restock cakes</button>
     </div>
--- react-rtk-demo/src/features/icecream/IcecreamView.jsx
+++ react-rtk-demo/src/features/icecream/IcecreamView.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export const IcecreamView = () => {
+  const numOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams);
   return (
     <div>
-      <h2>Number of Ice Creams</h2>
+      <h2>Number of Ice Creams - {numOfIcecreams}</h2>
       <button>Order Ice Cream</button>
       <button>Restock Ice Creams</button>
     </div>
