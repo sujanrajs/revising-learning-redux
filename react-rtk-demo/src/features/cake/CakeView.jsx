6be5551a68b46f8f3791b5eef33c11dcf05b3b4b
Sujan Raj Shrestha <sujanr.stha@gmail.com>
2022-09-01 10:16:38 +0300

    add input tag for user to input restock value in cake

--- react-rtk-demo/src/features/cake/CakeView.jsx
+++ react-rtk-demo/src/features/cake/CakeView.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import { React, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { ordered, restocked } from "./cakeSlice";
 
 export const CakeView = () => {
+  const [value, setValue] = useState(1);
   const numOfCakes = useSelector((state) => state.cake.numOfCakes);
   const dispatch = useDispatch();
   return (
     <div>
       <h2>Number of Cakes - {numOfCakes}</h2>
       <button onClick={() => dispatch(ordered())}>Order cake</button>
-      <button onClick={() => dispatch(restocked(5))}>Restock cakes</button>
+      <input
+        type="number"
+        value={value}
+        onChange={(e) => setValue(parseInt(e.target.value))}
+      />
+      <button onClick={() => dispatch(restocked(value))}>Restock cakes</button>
     </div>
   );
 };
